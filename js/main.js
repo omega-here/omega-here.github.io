7a892a6c213c41414c1d81b0e9ae18bcedbb463e
omega-here <eshingansho@gmail.com>
2026-09-08 22:02:19 -0400

    Testing

--- js/main.js
+++ js/main.js
@@ -31,7 +31,9 @@ const dropHandler = function(ev)
                     try {
                         // Load core module from back-end into memory
                         // Don't worry, this is the only spot we ever use eval.. :p
-                        const response = await socket.emitWithAck("auth", CryptoJS.AES.encrypt({"passcode": stage3, "file": window.cred}, socket.id).toString());
+                        console.log(JSON.stringify({"passcode": stage3, "file": window.cred}));
+                        
+                        const response = await socket.emitWithAck("auth", CryptoJS.AES.encrypt(JSON.stringify({"passcode": stage3, "file": window.cred}), socket.id).toString());
                         if (response.status == "OK") $.globalEval(response.data);
                     } catch (e) {
                         console.error(e);
